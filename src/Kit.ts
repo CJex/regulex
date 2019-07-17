8e9c1808d3b8357a693a2e662294564b8b53bb0b
CJ <i@jex.im>
2019-07-17 15:53:04 +0800

    Add Kit.CharRange

--- src/Kit.ts
+++ src/Kit.ts
@@ -277,3 +277,213 @@ export function enumNum(begin: number, end: number): number[] {
   }
   return a;
 }
+
+const _excludeSignCodePoint = 94; //ord('^');
+const _hyphenCodePoint = 45; // ord('-');
+const _escapeCodePoint = 92; // ord('\\');
+
+/** Math.pow(2,21). */
+const B2_21 = 0x200000;
+/** Math.pow(2,21) - 1. Bits Char.MAX_CODE_POINT used.  */
+const B2_21_1 = 0x1fffff;
+
+/**
+Inclusive CharRange representation.
+CharRangeRepr use number to save memory.
+Number.MAX_SAFE_INTEGER is 0x1fffffffffffff, 53 bit.
+Char.MAX_CODE_POINT is  0x10ffff, 21 bit.
+A number is sufficient to pack two char code points.
+```
+rangeBegin = CharRangeRepr / Math.pow(2,21) & 0x1FFFFF  // shift right 21 bit
+rangeEnd = CharRangeRepr &   0x1FFFFF // retain right 21 bit
+```
+A single code point use equal rangeBegin and rangeEnd.
+
+Use {_nominal_:true} to enforce nominal type check CharRangeRepr.
+Number can not directly assign to CharRangeRepr.
+
+@example:
+Single char "A" => `0x41 * Math.pow(2,21) + 0x41 === 0x8200041`
+Single char "Z" => `0x5A * Math.pow(2,21) + 0x5A === 0xB40005A`
+Range "A" to "Z" => `0x41 * Math.pow(2,21) + 0x5A === 0x820005A`
+
+@todo:  Use another bit to repr char range with even gap.
+"\x10\x12\x14\x16\x18" could be repr as range 0x10 to 0x18 but with even gap.
+*/
+export type CharRangeRepr = number & {_nominal_: true};
+
+export namespace CharRange {
+  /** Pack char range two chars to CharRangeRepr */
+  export function fromCharPair(beginChar: string, endChar: string): CharRangeRepr {
+    return pack(ord(beginChar), ord(endChar));
+  }
+
+  /** Pack single char to CharRangeRepr */
+  export function singleChar(c: string): CharRangeRepr {
+    return single(ord(c));
+  }
+
+  /** Pack single char code point to CharRangeRepr */
+  export function single(codePoint: number): CharRangeRepr {
+    return pack(codePoint, codePoint);
+  }
+
+  /** Pack char range two code points into number CharRangeRepr */
+  export function pack(begin: number, end: number): CharRangeRepr {
+    return (begin * B2_21 + end) as CharRangeRepr;
+  }
+
+  export function begin(r: CharRangeRepr): number {
+    return (r / B2_21) & B2_21_1;
+  }
+  export function end(r: CharRangeRepr): number {
+    return r & B2_21_1;
+  }
+
+  export function getSize(r: CharRangeRepr): number {
+    return end(r) - begin(r) + 1;
+  }
+
+  export function includeChar(range: CharRangeRepr, c: string): boolean {
+    let cp = ord(c);
+    return includeCodePoint(range, cp);
+  }
+
+  export function includeCodePoint(range: CharRangeRepr, cp: number): boolean {
+    return begin(range) <= cp && cp <= end(range);
+  }
+
+  export function isSubsetOf(a: CharRangeRepr, b: CharRangeRepr): boolean {
+    return begin(b) <= begin(a) && end(a) <= end(b);
+  }
+
+  export function isSingle(r: CharRangeRepr): boolean {
+    return begin(r) === end(r);
+  }
+
+  export function intersect(a: CharRangeRepr, b: CharRangeRepr): Maybe<CharRangeRepr> {
+    let a1 = begin(a);
+    let a2 = end(a);
+    let b1 = begin(b);
+    let b2 = end(b);
+    if (b2 < a1 || a2 < b1) return;
+    if (a1 <= b1) {
+      if (b2 <= a2) return b;
+      else return pack(b1, a2);
+    } else {
+      if (a2 <= b2) return a;
+      else return pack(a1, b2);
+    }
+  }
+
+  export function join(a: CharRangeRepr, b: CharRangeRepr): Maybe<CharRangeRepr> {
+    let a1 = begin(a);
+    let a2 = end(a);
+    let b1 = begin(b);
+    let b2 = end(b);
+    if (b2 + 1 < a1 || a2 + 1 < b1) return;
+    return pack(Math.min(a1, b1), Math.max(a2, b2));
+  }
+
+  /**
+  Subtract range:
+  a - b === a, if a and b have no overlap
+  a - b === undefined, if a isSubsetof b
+  a - b === range, if a starts with b or a ends with b
+  a - b === [r1,r2], if b isSubsetof a and a.begin < b.begin && b.end < a.end
+  */
+  export function subtract(a: CharRangeRepr, b: CharRangeRepr): Maybe<CharRangeRepr | [CharRangeRepr, CharRangeRepr]> {
+    let a1 = begin(a);
+    let a2 = end(a);
+    let b1 = begin(b);
+    let b2 = end(b);
+    if (b2 < a1 || a2 < b1) return a;
+    let left = a1 < b1;
+    let right = b2 < a2;
+    if (left && right) return [pack(a1, b1 - 1), pack(b2 + 1, a2)];
+    if (left) return pack(a1, b1 - 1);
+    if (right) return pack(b2 + 1, a2);
+    // if (!left && !right)  return;
+  }
+
+  /**
+  Coalesce char range list to non-overlapping sorted ranges.
+  */
+  export function coalesce(ranges: CharRangeRepr[]): CharRangeRepr[] {
+    let newRanges: CharRangeRepr[] = [];
+    ranges = sortUnique(ranges, CharRange.compare);
+    if (ranges.length <= 1) return ranges;
+
+    let prev = ranges[0];
+    for (let i = 1, l = ranges.length; i < l; i++) {
+      let a = ranges[i];
+      let joined = join(a, prev);
+      if (typeof joined === 'undefined') {
+        newRanges.push(prev);
+        prev = a;
+      } else {
+        prev = joined;
+      }
+    }
+
+    newRanges.push(prev);
+    return newRanges;
+  }
+
+  export function compare(a: CharRangeRepr, b: CharRangeRepr): Ordering {
+    if (a === b) return Ordering.EQ;
+    if (a < b) return Ordering.LT;
+    else return Ordering.GT;
+  }
+
+  export function compareIn(x: CharRangeRepr, r: CharRangeRepr): Ordering {
+    if (isSubsetOf(x, r)) return Ordering.EQ;
+    if (x < r) return Ordering.LT;
+    else return Ordering.GT;
+  }
+
+  /**
+  If char is less than range low bound, return LT, if char is in range, return EQ, and so forth.
+  */
+  export function compareCharToRange(c: string, range: CharRangeRepr): Ordering {
+    return compareCodePointToRange(ord(c), range);
+  }
+
+  export function compareCodePointToRange(cp: number, range: CharRangeRepr): Ordering {
+    let r1 = begin(range);
+    let r2 = end(range);
+    if (cp < r1) return Ordering.LT;
+    if (cp > r2) return Ordering.GT;
+    return Ordering.EQ;
+  }
+
+  /**
+  Convert CharRange to regex char class like pattern string
+  @see `Charset.fromPattern`
+  */
+  export function toPattern(range: CharRangeRepr): string {
+    let r1 = begin(range);
+    let r2 = end(range);
+    if (r1 === r2) return toChar(r1);
+    if (r1 + 1 === r2) return toChar(r1) + toChar(r2);
+    return toChar(r1) + '-' + toChar(r2);
+
+    function toChar(cp: number): string {
+      if (cp === _escapeCodePoint || cp === _excludeSignCodePoint || cp === _hyphenCodePoint) {
+        return '\\' + chr(cp);
+      } else {
+        return chr(cp);
+      }
+    }
+  }
+
+  export function toCodePoints(range: CharRangeRepr, maxCount = Infinity): number[] {
+    let r1 = begin(range);
+    let r2 = end(range);
+    let a = [];
+    while (r1 <= r2 && a.length < maxCount) {
+      a.push(r1++);
+    }
+    return a;
+  }
+}
--- test/KitSpec.ts
+++ test/KitSpec.ts
@@ -4,6 +4,14 @@ import {testProp} from './utils';
 
 import assert = require('assert');
 
+const charPairGen = () => C.tuple(C.fullUnicode(), C.fullUnicode());
+const charRangeGen = () =>
+  charPairGen().map(a => {
+    a.sort(K.compareFullUnicode);
+    return K.CharRange.fromCharPair(a[0], a[1]);
+  });
+const listOfCharRange = (max = 40) => C.array(charRangeGen(), 1, max);
+
 describe('Kit', () => {
   describe('bsearch', () => {
     testProp('should be found', C.array(C.integer()), a => {
@@ -46,6 +54,52 @@ describe('Kit', () => {
       assert(b.length === n);
       for (let i = 0; i < a.length; i++) {
         assert(K.bsearch(b, a[i]).found);
+      }
+    });
+  });
+
+  describe('CharRange', () => {
+    it('compareFullUnicode', () => {
+      // String.fromCodePoint(0x1F437) == "🐷" == "\uD83D\uDC37" == "\u{1F437}"
+      let c1 = '\uD83D\uDC37';
+      let c2 = '\uFA16';
+      assert(K.compareFullUnicode(c1, c2) > 0);
+    });
+
+    testProp('subtract join intersect', C.tuple(charRangeGen(), charRangeGen()), a => {
+      let [r1, r2] = a;
+      assert(K.CharRange.subtract(r1, r1) === undefined);
+      assert(K.CharRange.join(r1, r1) === r1);
+
+      let inter = K.CharRange.intersect(r1, r2);
+      if (inter === undefined) {
+        assert(K.CharRange.subtract(r1, r2) === r1);
+        assert(K.CharRange.subtract(r2, r1) === r2);
+      } else {
+        let join = K.CharRange.join(r1, r2);
+        if (join === undefined) return assert(join !== undefined);
+
+        let j1 = K.CharRange.subtract(join, r1);
+        if (j1 === undefined) return assert(join === r1 && K.CharRange.isSubsetOf(r2, r1));
+        let j2 = K.CharRange.subtract(join, r2);
+        if (j2 === undefined) return assert(join === r2 && K.CharRange.isSubsetOf(r1, r2));
+
+        if (Array.isArray(j1)) {
+          assert(inter === r1);
+        } else if (Array.isArray(j2)) {
+          assert(inter === r2);
+        } else {
+          assert(K.CharRange.join(j1, inter) === r2);
+          assert(K.CharRange.join(j2, inter) === r1);
+        }
+      }
+    });
+
+    testProp('should include after coalesce', listOfCharRange(), a => {
+      let ranges = K.CharRange.coalesce(a);
+      for (let r of a) {
+        let {found, index} = K.bsearch(ranges, r, K.CharRange.compareIn);
+        assert(found && K.CharRange.isSubsetOf(r, ranges[index]));
       }
     });
   });
